ae61c8e432b1caff2febb239303095b49d18006d
evgeniizab <evgenii@zabairachnyi.com>
2021-11-24 19:34:08 +0400

    init: create new module

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © 2021 Evgenii e@zab.bz
+ * See LICENSE for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    "EvgeniiZab_YearCalendar",
+    __DIR__
+);
